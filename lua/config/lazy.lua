b02eb16b4b32dc9ca6c5662f423e40404484b505
Bretan-Cezar <cezar.bretan@gmail.com>
2024-01-24 23:31:32 +0200

    fugitive config

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -16,74 +16,10 @@ vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
 	
 	{ 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
-	{ 'rebelot/kanagawa.nvim', lazy = false, config = function() vim.cmd("colorscheme kanagawa-dragon") end },
-	{ 'nvim-treesitter/nvim-treesitter', 
-	   build = function() 
-	   	local install = require("nvim-treesitter.install")
 
-		install.update({ with_sync = true })()
-		install.compilers = { "cl" }
-	   
-	   end, 
-	   config = function()
-		local configs = require("nvim-treesitter.configs")
+	{ 'rebelot/kanagawa.nvim', lazy = false, config = function() vim.cmd("colorscheme kanagawa-wave") end },
+	
+	{ 'nvim-treesitter/nvim-treesitter', build = ":TSUpdate" },
 
-		configs.setup({
-
-			ensure_installed = {
---				"angular", 
---				"bash", 
---				"c", 
---				"c_sharp", 
---				"cmake", 
---				"cpp", 
---				"css", 
---				"dart", 
---				"dockerfile",
---				"elixir",
---				"gitignore",
---				"go",
---				"gomod",
---				"gosum",
---				"gowork",
---				"graphql",
---				"groovy",
---				"haskell",
---				"html",
---				"java",
---				"javascript",
---				"json",
---				"kotlin",
---				"latex",
---				"lua",
---				"make",
---				"markdown",
---				"nasm",
---				"printf",
---				"prisma",
---				"properties",
---				"python",
---				"r",
---				"regex",
---				"rust",
---				"scala",
---				"scss",
---				"solidity",
---				"sql",
---				"svelte",
---				"swift",
---				"toml",
---				"tsx",
---				"typescript",
---				"vue",
---				"xml",
---				"yaml",
---				"zig"
-			},
-			sync_install = false,
-			highlight = {enable = true},
-			indent = { enable = true }
-		})
-		
-	end}
+	'tpope/vim-fugitive'
 })
